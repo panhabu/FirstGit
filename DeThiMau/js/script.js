9ba0ad43389a19d0dfb21a4ff6afc37b33ac41f3
panhabu <144882907+panhabu@users.noreply.github.com>
2023-10-23 15:34:01 +0700

    Add files via upload

--- DeThiMau/js/script.js
+++ DeThiMau/js/script.js
@@ -1,2 +1,31 @@
 // coding implementation
+document.getElementById('submit').onclick = send
+function send(){
+    var email = document.getElementById("email").value;
+    var comment = document.getElementById("com").value;
+    var rating = document.getElementById("rate").value;
+
+    var emailPattern = /^[a-zA-Z0-9._-]+@gmail\.com$/;
+
+    if(email.trim() === ""){
+        alert("Email không được để trống");
+        return;
+    }
+    if (!email.match(emailPattern)) {
+        alert("Email không hợp lệ");
+        return false;
+    }
+
+    if (isNaN(rating) || rating.trim() === "") {
+        alert("Rating phải là một số");
+        return false ;
+    }
+
+
+    if (comment.trim() === "") {
+        alert("Comment không được để trống");
+        return false;
+    }
+}
+
 // end coding implementation
